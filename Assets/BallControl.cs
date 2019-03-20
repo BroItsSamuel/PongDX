e2db2ca074ecb293a183422c1f550c4537cf37e9
Samuel White <40038084+BroItsSamuel@users.noreply.github.com>
2019-03-20 13:53:34 -0700

    Initial Commit

---
+++ Assets/BallControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+
+    private Rigidbody2D _rb2D;
+
+    void GoBall() {
+        float rand = Random.Range (0, 2);
+        if (rand < 1) {
+            _rb2D.AddForce (new Vector2 (55, -40));
+        } else {
+            _rb2D.AddForce (new Vector2 (-55, -40));
+        }
+    }
+
+    void Start () {
+        _rb2D = GetComponent<Rigidbody2D> ();
+        Invoke ("GoBall", 2);
+    }
+
+    void ResetBall() {
+        _rb2D.velocity = new Vector2 (0, 0);
+        transform.position = Vector2.zero;
+    }
+
+    void RestartGame() {
+        ResetBall ();
+        Invoke ("GoBall", 1);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        if (coll.collider.CompareTag ("Player")) {
+            Vector2 vel;
+            vel.x = _rb2D.velocity.x;
+            vel.y = (_rb2D.velocity.y / 2.0f) + (coll.collider.attachedRigidbody.velocity.y / 3.0f);
+            _rb2D.velocity = vel;
+        }
+    }
+
+}
---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+    public float speed = 10.0f;
+    public float boundY = 4.5f;
+    private Rigidbody2D _rb2D;
+    void Start () {
+        _rb2D = GetComponent<Rigidbody2D>();
+    }
+    void Update () {
+        var vel = _rb2D.velocity;
+        if (Input.GetKey(moveUp)) {
+            vel.y = speed;
+        }
+        else if (Input.GetKey(moveDown)) {
+            vel.y = -speed;
+        }
+        else {
+            vel.y = 0;
+        }
+        _rb2D.velocity = vel;
+
+        var pos = transform.position;
+        if (pos.y > boundY) {
+            pos.y = boundY;
+        }
+        else if (pos.y < -boundY) {
+            pos.y = -boundY;
+        }
+        transform.position = pos;
+    }
+}
+   
+
